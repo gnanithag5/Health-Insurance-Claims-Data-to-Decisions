41967764439b957aecf850c4b715f99cd6bfa556
Gnanitha Garikipati <gnanithagarikipati@gmail.com>
2025-09-26 12:58:33 -0600

    Delete models directory

--- models/marts/fraud_analysis/fct_claim_diagnoses.sql
+++
@@ -1,9 +0,0 @@
-{{ config(schema='MARTS_FRAUD_ANALYSIS', materialized='table') }}
-
-select
-    claimid,
-    beneid,
-    provider,
-    claim_type,
-    diagnosis_code
-from {{ ref('int_claims_diagnosis_long') }}
--- models/marts/fraud_analysis/fct_claim_procedures.sql
+++
@@ -1,9 +0,0 @@
-{{ config(schema='MARTS_FRAUD_ANALYSIS', materialized='table') }}
-
-select
-    claimid,
-    beneid,
-    provider,
-    claim_type,
-    procedure_code
-from {{ ref('int_claims_procedure_long') }}
